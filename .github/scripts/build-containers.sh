b718c8822650ad18f217a1cd2037bcf0cfedd683
Miguel Ferreira <miguelferreira@me.com>
2020-05-23 18:42:26 +0200

    Extract container build to script and use it to build tags

---
+++ .github/scripts/build-containers.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euov pipefail
+IFS=$'\n\t'
+
+usage() {
+    echo "==# Expected docker image tag as 1st arg"
+    exit 1
+}
+
+git fetch origin master
+short_sha=$(  git rev-parse --short HEAD )
+docker_image_tag=${1-${short_sha}}
+
+[[ -z "${docker_image_tag}" ]] && usage
+
+for container in $( ls docker ); do
+    container_directory="docker/${container}"
+
+    if [[ -d "${container_directory}" ]] && ! git diff --quiet $( git merge-base origin/master HEAD ) HEAD -- ${container_directory}; then
+        cd ${container_directory}
+        docker_image_full_path=$(echo docker.pkg.github.com/${GITHUB_REPOSITORY}/${container}:${docker_image_tag} | tr '[:upper:]' '[:lower:]')
+        docker login -u publisher -p ${DOCKER_TOKEN} docker.pkg.github.com
+        eval "docker build -t ${docker_image_full_path} ."
+        docker push ${docker_image_full_path}
+        cd -
+    fi
+done
